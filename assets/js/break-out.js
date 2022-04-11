b7567a543a94cd9403054fb3e2c4ac1a0af48292
Nigel Doheny <nigel.doheny93@gmail.com>
2022-04-11 19:00:48 +0000

    At 1:41:28 of YT video

---
+++ assets/js/break-out.js
@@ -0,0 +1,40 @@
+const grid = document.querySelector('.b-grid');
+const blockWidth = 100;
+const blockHeight = 20;
+
+//create block
+class Block{
+    constructor(xAxis, yAxis) {
+    this.bottomLeft = [xAxis, yAxis];
+    this.bottomRight = [xAxis + blockWidth, yAxis]
+    this.topLeft = [xAxis, yAxis + blockHeight]
+    this.topRight = [xAxis + blockWidth, yAxis + blockHeight]
+    }
+}
+// all my blocks
+const blocks = [
+   
+    new Block(120,270),
+    new Block(230,270),
+    new Block(340,270),
+    new Block(450,270),
+   
+]
+
+
+
+// draw my block
+
+function addBlock(){
+ for(let i = 0; i < blocks.length; i++){
+
+    const block = document.createElement('div');
+    block.classList.add('block');
+    block.style.left = blocks[i].bottomLeft[0] + 'px';
+    block.style.bottom = blocks[i].bottomLeft[1] + 'px';
+    grid.appendChild(block);
+  }
+}
+
+
+addBlock();
