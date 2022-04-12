7106f330e20acf525adcc9583d58bf7fb5ff6a26
Nigel Doheny <nigel.doheny93@gmail.com>
2022-04-12 10:57:00 +0000

    2:11:22 on YT video with Ania Kubow

--- assets/js/break-out.js
+++ assets/js/break-out.js
@@ -1,6 +1,21 @@
 const grid = document.querySelector('.b-grid');
 const blockWidth = 100;
 const blockHeight = 20;
+const boardWidth = 470;
+
+const ballDiameter = 20;
+let xDirection = 2;
+let yDirection = 2;
+
+let timerId;
+
+
+const userStart = [230,10];
+let currentPostion = userStart;
+
+const ballStart= [270,40]
+let ballCurrentPostion =ballStart;
+
 
 //create block
 class Block{
@@ -13,11 +28,23 @@ class Block{
 }
 // all my blocks
 const blocks = [
-   
+    new Block(10,270),
     new Block(120,270),
     new Block(230,270),
     new Block(340,270),
     new Block(450,270),
+
+    new Block(10,240),
+    new Block(120,240),
+    new Block(230,240),
+    new Block(340,240),
+    new Block(450,240),
+
+    new Block(10,210),
+    new Block(120,210),
+    new Block(230,210),
+    new Block(340,210),
+    new Block(450,210),
    
 ]
 
@@ -25,7 +52,7 @@ const blocks = [
 
 // draw my block
 
-function addBlock(){
+function addBlocks(){
  for(let i = 0; i < blocks.length; i++){
 
     const block = document.createElement('div');
@@ -35,6 +62,92 @@ function addBlock(){
     grid.appendChild(block);
   }
 }
+addBlocks();
+
+//add user
+const user = document.createElement('div');
+user.classList.add('user');
+drawUser();
+grid.appendChild(user);
 
 
-addBlock();
+// draw the User
+function drawUser(){
+  user.style.left =  currentPostion[0] + 'px';
+  user.style.bottom = currentPostion[1] + 'px';
+}
+
+
+// move user
+function moveUser(event){
+  switch(event.key){
+    case 'ArrowLeft':
+      if(currentPostion[0] > 0){
+      currentPostion[0] -= 10;
+      drawUser();
+      }
+    break;
+  }
+  switch(event.key){
+    case 'ArrowRight':
+      if(currentPostion[0] < 470){
+      currentPostion[0] += 10;
+       drawUser();
+      }
+    break;
+  }
+  switch(event.key){
+    case 'ArrowUp':
+    currentPostion[1] += 1;
+    drawUser();
+    break;
+  }
+  switch(event.key){
+    case 'ArrowDown':
+    currentPostion[1] -= 1;
+    drawUser();
+    break;
+  }
+}
+
+document.addEventListener('keydown',moveUser);
+
+
+// draw the ball
+function drawBall(){
+  ball.style.left = ballCurrentPostion[0] +'px';
+  ball.style.bottom =  ballCurrentPostion[1] +'px'
+}
+
+// add ball
+
+const ball = document.createElement('div');
+ball.classList.add('ball');
+drawBall();
+grid.appendChild(ball);
+
+//move the ball
+
+function moveBall(){
+  ballCurrentPostion[0] += xDirection;
+  ballCurrentPostion[1] += yDirection;
+  drawBall()
+  checkForCollision()
+}
+ timerId = setInterval(moveBall, 30);
+ 
+ // check for collision
+ function checkForCollision(){
+   // check for the wall collision
+   if(ballCurrentPostion[0] >= (boardWidth - ballDiameter)){
+     changeDirection();
+   }
+ }
+
+ function changeDirection(){
+      if(xDirection === 2 && yDirection == 2){
+        yDirection = -2;
+        return;
+      }
+      //if()
+ }
